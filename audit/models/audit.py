ac34cfda1675e78e965a995b4e7c272a87f64e1d
tesham <tesham29@gmail.com>
2024-06-24 03:03:11 +0600

    :construction: audit model added

---
+++ audit/models/audit.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Audit(models.Model):
+    user = models.CharField(max_length=100)
+    session_id = models.IntegerField(null=True)
+    label = models.CharField(max_length=100)
+    ip = models.CharField(max_length=100)
+    action = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'audits'
